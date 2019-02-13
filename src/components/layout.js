14a4e87cd272a32a0c180dafeb8848e89f425abb
ShubhamModi004 <shubhammodi004@gmail.com>
2019-02-13 09:53:35 +0530

    Navbar done

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,19 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
+import Navbar from './Globals/Navbar'
+
 import './bootstrap.min.css';
 import "./layout.css";
 
 
-const Layout = ({ children }) => <>{children}</>;
+const Layout = ({ children }) => (
+
+  <>
+    <Navbar />
+    {children}
+  </>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
